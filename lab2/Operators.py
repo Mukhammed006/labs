4f962baea9b1855d42dedb301112a29b29e5fcce
Mukhammed006 <myhagarifolla123456@gmail.com>
2025-01-29 14:26:40 +0600

    1234

---
+++ lab2/Operators.py
@@ -0,0 +1,28 @@
+#ex1
+print(10 + 5)
+#ex2
+print((6 + 3) - (6 + 3))
+#ex3
+print(100 + 5 * 3)
+#ex4
+print(5 + 4 - 7 + 3)
+
+#Ex1 Multiply
+print(10 * 5)
+
+#Ex2 Divide
+print(10 * 2)
+
+#Ex3 
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+  print("Yes, apple is a fruit!")
+
+#Ex4
+if 5 != 10:
+  print("5 and 10 is not equal")
+
+#Ex5
+if 5 == 10 or 4 == 4:
+  print("At least one of the statements is true")
+  
