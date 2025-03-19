e4f5b983fd963201a57d1f3e7fa710994b2258f5
Mukhammed006 <myhagarifolla123456@gmail.com>
2025-03-19 14:56:57 +0600

    .

--- lab7/circle.py
+++ lab7/circle.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800,600))
-pygame.display.set_caption('Nurlan_Games')
+pygame.display.set_caption('Games')
 
 ball_position = [400, 300]
 speed = 20
